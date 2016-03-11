ec557e331e9abcfe0164faec70f666b9d903875c
zapion <zapionator@gmail.com>
2016-03-11 17:42:55 +0800

    adding more handlers; dump attribute json file

--- apoy/rule_analysis/rule_analysis.py
+++ apoy/rule_analysis/rule_analysis.py
@@ -4,15 +4,30 @@ import shutil
 from framework import FrameworkParser
 
 
+def find_all(path, target):
+    res = []
+    for root, dirs, fnames in os.walk(path):
+        for fname in fnames:
+            for line, content in enumerate(open(os.path.join(root, fname))):
+                # case insensitive
+                content = content.lower()
+                if target in content:
+                    res.append("{0} found in line: {1} of file: {2}".format(
+                                target, line + 1, fname))
+    return res
+
+
 def find(path, target):
     res = []
     for root, dirs, fnames in os.walk(path):
         for fname in fnames:
             for line, content in enumerate(open(os.path.join(root, fname))):
+                # case insensitive
+                content = content.lower()
                 if target in content:
                     res.append("{0} found in line: {1} of file: {2}".format(
                                 target, line + 1, fname))
-    return res
+                    return res
 
 
 def login_handler(repo_summary):
@@ -31,6 +46,22 @@ def forgot_password_handler(repo_summary):
     return False
 
 
+def todo_handler(repo_summary):
+    path = repo_summary['repo_path']
+    res = find(path, 'todo')
+    if res:
+        return True
+    return False
+
+
+def register_handler(repo_summary):
+    path = repo_summary['repo_path']
+    res = find(path, 'register')
+    if res:
+        return True
+    return False
+
+
 # predefined rule handlers
 rule_handlers = {'login': login_handler,
                  'forgot_password': forgot_password_handler,
@@ -38,8 +69,10 @@ rule_handlers = {'login': login_handler,
                  'angular': FrameworkParser.angular_handler,
                  }
 
+
 def cleanup(input_path):
     shutil.rmtree(input_path)
+
 
 def run_analysis(repo_summary):
     repo_path = repo_summary['repo_path']
@@ -47,14 +80,13 @@ def run_analysis(repo_summary):
     enabled_attributes = {k: True for k, v in rule_handlers.items()
                           if v(repo_summary)}
     cleanup(repo_path)
-    print("enabled attributes: ")
+    print("enabled attributes: {}".format(enabled_attributes))
     return enabled_attributes
 
 
 def main():
     # please git clone Repo before running this testing...
     repo_path = '/temp/angular-registration-login-example'
-    #repo_path = '/temp/todomvc/examples/react'
     return run_analysis({'repo_path': repo_path})
 
 if __name__ == '__main__':
--- apoy/task_worker.py
+++ apoy/task_worker.py
@@ -1,5 +1,6 @@
 import os
 import pprint
+import json
 from threading import Thread
 from rule_analysis.rule_analysis import run_analysis
 from dcc.dccCaseFilter import DccCaseLibGenerator
@@ -13,18 +14,33 @@ class TaskWorker(Thread):
         self.project_repo_summary = project_repo_summary
 
     def run(self):
-        # TODO: not implemented
+        case_json_dir = os.path.join(os.getcwd(), "results")
         print('TaskID: {}'.format(self.taskid))
         pp = pprint.PrettyPrinter(indent=4)
         pp.pprint(self.project_repo_summary)
         attribute_list = run_analysis(self.project_repo_summary)
-        pp.pprint(attribute_list)
 
-        case_csv_fp = os.path.join(os.getcwd(), "dcc", "tmp", str(self.taskid) + ".csv")
+        attr_json_fp = os.path.join(case_json_dir,
+                                    str(self.taskid) + "_summary.json")
+        dump_target = self.project_repo_summary.copy()
+        dump_target["enabled_attributes"] = attribute_list.copy()
+        self.dump_to_file(
+            dump_target, attr_json_fp)
+
+        case_csv_fp = os.path.join(os.getcwd(),
+                                   "dcc",
+                                   "tmp",
+                                   str(self.taskid) + ".csv")
         obj_case_filter = DccCaseLibGenerator(attribute_list, case_csv_fp)
         obj_case_filter.run()
-        case_json_fp = os.path.join(os.getcwd(), "results", str(self.taskid) + ".json")
+        case_json_fp = os.path.join(case_json_dir, str(self.taskid) + ".json")
         obj_dcc_compiler = DccCompiler(case_csv_fp, case_json_fp)
         obj_dcc_compiler.run()
 
-
+    @staticmethod
+    def dump_to_file(data, output_path):
+        dirname = os.path.dirname(output_path)
+        if not os.path.exists(dirname):
+            os.mkdir(dirname)
+        with open(output_path, 'wb') as write_fh:
+            json.dump(data, write_fh)
